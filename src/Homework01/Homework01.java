4fa9ae39e9dc32890c161298d9aa22021a04766c
Barry <pingwen851022@gmail.com>
2024-09-30 16:06:29 +0800

    first commit

---
+++ src/Homework01/Homework01.java
@@ -0,0 +1,57 @@
+package Homework01;
+
+public class Homework01 {
+	
+	public static void main(String[] args) {
+		
+		// 1-1 計算12,6這兩個數值的和與積
+//		int num1 = 6, num2 = 12;
+//		int sum = num1 + num2;
+//		int product = num1 * num2;
+//		
+//		System.out.println(" 6+12 = " + sum);
+//		System.out.println(" 6*12 = " + product);
+		
+		// 1-2 計算200顆蛋共是幾打幾顆?(一打12顆)
+//		int egg = 200, dozen = 12;
+//		int total = egg / dozen;
+//		int remainder = egg % dozen;
+//		
+//		System.out.println(" 200顆蛋總共是 " + total + '打' + remainder + '顆');
+		
+		// **1-3 算出256559秒為多少天、多少小時、多少分與多少秒
+//		int total = 256559;
+//		int m = 60, h = 60 * 60, d = 60 * 60 * 24;
+//		int d1 = total / d;
+//		int h1 = total % h;
+//		int m1 = total % m;
+//		int s = total % d1;
+//	
+//		System.out.println( d1 + "天" + h1 + "時" + m1 + "分" + s + "秒");
+//		
+		// 1-4 請定義一個常數為3.1415(圓周率),並計算半徑為5的圓面積與圓周長
+//		double p = 3.1425;
+//		int pi = 5;
+//		double area = pi * pi * p;
+//		double cirp = pi * 2 * p;
+//		
+//		System.out.println(" 圓面積 = " + area);
+//		System.out.println(" 圓周長 = " + cirp);
+	
+		
+		// 1-5某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,請用程式計算10年後,本
+        // 金加利息共有多少錢? 複利計算公式:複利計算公式：本利和 = 本金 × (1 + 年利率)^期間
+//		int principal = 1500000;
+//		int total = principal * (1 + 0.02)^10;
+		
+		
+		// 1-6 請寫一隻程式,利用System.out.println()印出以下三個運算式結果:
+		// 5 + 5
+		// 5 + ‘5
+		// 5 + “5”
+		// 並請用註解各別說明答案的產生原因
+	}
+	
+		
+		
+}
